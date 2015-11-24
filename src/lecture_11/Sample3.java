bbd284857560a7ef28231ad0e6595eb981b87607
alex-icez <alex-icex@mail.ru>
2015-11-24 20:40:05 +0300

    mess

--- src/lecture_11/Sample3.java
+++ src/lecture_11/Sample3.java
@@ -1,10 +1,23 @@
 package lecture_11;
 
+
+
+
+
 public class Sample3 {
 
+	static void print() {
+		System.out.print("[ Hello ");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		}
+		System.out.println("World ]");
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
-
 }
