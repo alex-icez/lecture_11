9155d5a56e4602afc1887c6c60b7b720f59aec01
alex-icez <alex-icex@mail.ru>
2015-11-24 18:39:32 +0300

    mess

--- src/lecture_11/Sample.java
+++ src/lecture_11/Sample.java
@@ -1,8 +1,11 @@
 package lecture_11;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class Sample {
 	public static void main(String[] args) {
-		
+		HashMap<Integer, LinkedList<String>> map = new HashMap<>();
 
 	}
 }
