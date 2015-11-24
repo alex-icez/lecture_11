730ee8e611a27d80d3073b23e2961c0a6602331b
alex-icez <alex-icex@mail.ru>
2015-11-24 19:04:14 +0300

    mess

--- src/lecture_11/Sample2.java
+++ src/lecture_11/Sample2.java
@@ -10,7 +10,7 @@ class MyThread extends Thread {
 		for(int i = 0; i < 100; i++) {
 			System.out.println(this.getName() + " " + i);
 			try {
-				Thread.sleep(System.currentTimeMillis() % 200);
+				//Thread.sleep(System.currentTimeMillis() % 200);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -21,8 +21,8 @@ class MyThread extends Thread {
 public class Sample2 {
 
 	public static void main(String[] args) {
-		MyThread th1 = new MyThread();
-		MyThread th2 = new MyThread();
+		MyThread th1 = new MyThread("first");
+		MyThread th2 = new MyThread("second");
 		th1.start();
 		th2.start();
 	}
