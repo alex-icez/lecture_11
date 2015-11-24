5b0de6e937c9fb950c8993326e051b523b5e76f5
alex-icez <alex-icex@mail.ru>
2015-11-24 18:58:47 +0300

    mess

--- src/lecture_11/Sample2.java
+++ src/lecture_11/Sample2.java
@@ -2,9 +2,14 @@ package lecture_11;
 
 class MyThread extends Thread {
 	public void run() {
-		for(int i = 0; i < 100; i++)
+		for(int i = 0; i < 100; i++) {
 			System.out.println(i);
-		
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
 
