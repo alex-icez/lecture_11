6c7e137eabf406b40f19f6b44e91c496f675c8d7
alex-icez <alex-icex@mail.ru>
2015-11-24 19:03:41 +0300

    mess

--- src/lecture_11/Sample2.java
+++ src/lecture_11/Sample2.java
@@ -1,6 +1,11 @@
 package lecture_11;
 
 class MyThread extends Thread {	
+	
+	public MyThread(String name) {
+		super(name);
+	}
+	
 	public void run() {
 		for(int i = 0; i < 100; i++) {
 			System.out.println(this.getName() + " " + i);
