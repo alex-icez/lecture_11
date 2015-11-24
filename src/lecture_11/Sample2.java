999d13c6d04f15ee01c80f13b64aca4e7e68051a
alex-icez <alex-icex@mail.ru>
2015-11-24 18:59:54 +0300

    mess

--- src/lecture_11/Sample2.java
+++ src/lecture_11/Sample2.java
@@ -13,12 +13,11 @@ class MyThread extends Thread {
 	}
 }
 
-
-
 public class Sample2 {
 
 	public static void main(String[] args) {
-		
+		MyThread th1 = new MyThread();
+		MyThread th2 = new MyThread();
 
 	}
 
