89da15507e003cd806b52749f32b5e62070ab12a
alex-icez <alex-icex@mail.ru>
2015-11-24 18:28:46 +0300

    mess

--- src/lecture_11/Sample1.java
+++ src/lecture_11/Sample1.java
@@ -14,12 +14,22 @@ class Tree<T extends Comparable<T>> {
 	
 	Node root = null;
 	
+	
+	private void add(Node iter, Node node) {
+		if (iter.value.compareTo(node.value) > 0) {
+			if (iter.left != null)
+				add(iter.left, node);
+			else
+				iter.left = node;
+		}
+		
+	}
+	
 	void add(T value) {
 		Node node = new Node(value);
 		if (root == null) {
 			root = node;
 			return;
-			
 		}
 		
 		
